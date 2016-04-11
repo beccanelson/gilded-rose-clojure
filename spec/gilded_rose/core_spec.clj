666ff7e7c0fafc2a0f20c461b2b70ccab32d64f3
Becca Nelson <beccanelson88@gmail.com>
2016-04-11 16:05:01 -0500

    Add variable names for items and test sell-in value

--- spec/gilded_rose/core_spec.clj
+++ spec/gilded_rose/core_spec.clj
@@ -1,6 +1,6 @@
 (ns gilded-rose.core-spec
 (:require [speclj.core :refer :all]
-          [gilded-rose.core :refer [update-quality item]]))
+          [gilded-rose.core :refer [update-quality item update-current-inventory]]))
 
 (describe "gilded rose"
   (describe "item"
@@ -14,15 +14,25 @@
       (it "has a quality"
         (should= (sample-item :quality) 20))))
 
-    (describe "update quantity"
-      (let [any-item (item "Default Item" 10 20) sulfuras (item "Sulfuras, Hand of Ragnaros" 0 80)]
+  (describe "update current inventory"
+    (let [updated-inventory (update-current-inventory)]
+      (let [
+        dexterity-vest (nth updated-inventory 0)
+        aged-brie (nth updated-inventory 1) elixir-mongoose (nth updated-inventory 2)
+        sulfuras (nth updated-inventory 3) backstage-passes (nth updated-inventory 4)]
 
-        (update-quality [any-item sulfuras])
-
-        (it "lowers the sell-in value at the end of the day"
-          (should= (any-item :sell-in) 9))
+        (it "lowers the sell-in value at the end of the day for all items except 'Sulfuras'"
+          (should= (dexterity-vest :sell-in) 9)
+          (should= (aged-brie :sell-in) 1)
+          (should= (elixir-mongoose :sell-in) 4)
+          (should= (backstage-passes :sell-in) 14))
 
         (it "does not lower the sell-in value if the item is named 'Sulfuras, Hand of Ragnaros'"
-          (should= (sulfuras :sell-in) 0))))
+          (should= (sulfuras :sell-in) 0))
+
+
+
+      )))
+
 
 )
