52992fbc781d7718c1ba74bed46f17e28c6e015a
Becca Nelson <beccanelson88@gmail.com>
2016-04-11 15:21:30 -0500

    Add tests for sell-in value

--- spec/gilded_rose/core_spec.clj
+++ spec/gilded_rose/core_spec.clj
@@ -7,9 +7,22 @@
     (let [sample-item (item "Sample Item" 10 20)]
       (it "has a name"
         (should= (sample-item :name) "Sample Item"))
+
       (it "has a sell-in value"
         (should= (sample-item :sell-in) 10))
-      (it "has a quality"
-        (should= (sample-item :quality) 20)))
 
-))
+      (it "has a quality"
+        (should= (sample-item :quality) 20))))
+
+    (describe "update quantity"
+      (let [any-item (item "Default Item" 10 20) sulfuras (item "Sulfuras, Hand of Ragnaros" 0 80)]
+
+        (update-quality [any-item sulfuras])
+
+        (it "lowers the sell-in value at the end of the day"
+          (should= (any-item :sell-in) 9))
+
+        (it "does not lower the sell-in value if the item is named 'Sulfuras, Hand of Ragnaros'"
+          (should= (sulfuras :sell-in) 0))))
+
+)
