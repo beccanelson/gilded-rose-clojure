5b273003bf06441703f092dae4351ec76f256431
Becca Nelson <beccanelson88@gmail.com>
2016-04-11 16:22:08 -0500

    re-factor existing tests to group by item name

--- spec/gilded_rose/core_spec.clj
+++ spec/gilded_rose/core_spec.clj
@@ -21,24 +21,29 @@
         aged-brie (nth updated-inventory 1) elixir-mongoose (nth updated-inventory 2)
         sulfuras (nth updated-inventory 3) backstage-passes (nth updated-inventory 4)]
 
-        (it "lowers the sell-in value at the end of the day for all items except 'Sulfuras'"
-          (should= (dexterity-vest :sell-in) 9)
-          (should= (aged-brie :sell-in) 1)
-          (should= (elixir-mongoose :sell-in) 4)
-          (should= (backstage-passes :sell-in) 14))
+        (describe "default items"
+          (it "lowers the sell-in value by 1"
+            (should= (dexterity-vest :sell-in) 9)
+            (should= (aged-brie :sell-in) 1)
+            (should= (elixir-mongoose :sell-in) 4)
+            (should= (backstage-passes :sell-in) 14))
 
-        (it "does not lower the sell-in value if the item is named 'Sulfuras, Hand of Ragnaros'"
-          (should= (sulfuras :sell-in) 0))
-          ; this test fails, returns -1
-
-        (it "lowers the quality by 1 on default items"
-          (should= (dexterity-vest :quality) 19)
-          (should= (elixir-mongoose :quality) 6))
-
-        (it "increases in quality if name is Aged Brie"
-          (should= (aged-brie :quality) 1))
-
-      )))
+          (it "lowers the quality by 1 if sell-in has not passed"
+            (should= (dexterity-vest :quality) 19)
+            (should= (elixir-mongoose :quality) 6))
+          )
 
 
-)
+        (describe "Aged Brie"
+          (it "increases in quality"
+            (should= (aged-brie :quality) 1))
+        )
+
+        (describe "Sulfuras"
+          (it "never lowers it sell-in value"
+            (should= (sulfuras :sell-in) 0))
+
+          (it "never decreases in quality"
+            (should= (sulfuras :quality) 80))
+        )
+))))
