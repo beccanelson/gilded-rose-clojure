a3c05b5ed976dd2942aa156865e7b4401386ae6b
Becca Nelson <beccanelson88@gmail.com>
2016-04-11 13:55:10 -0500

    Add initial test for item attributes

--- spec/gilded_rose/core_spec.clj
+++ spec/gilded_rose/core_spec.clj
@@ -1,6 +1,15 @@
 (ns gilded-rose.core-spec
 (:require [speclj.core :refer :all]
-          [gilded-rose.core :refer [update-quality]]))
+          [gilded-rose.core :refer [update-quality item]]))
 
 (describe "gilded rose"
-          (it "TODO - write tests later"))
+  (describe "item"
+    (let [sample-item (item "Sample Item" 10 20)]
+      (it "has a name"
+        (should= (sample-item :name) "Sample Item"))
+      (it "has a sell-in value"
+        (should= (sample-item :sell-in) 10))
+      (it "has a quality"
+        (should= (sample-item :quality) 20)))
+
+))
