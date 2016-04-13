ec156c94119c0452ce49f15244c7ef269f28c77d
Becca Nelson <beccanelson88@gmail.com>
2016-04-13 13:55:11 -0500

    edit test syntax and add exception to update-sell-in and update-quality for Sulfuras

--- spec/gilded_rose/core_spec.clj
+++ spec/gilded_rose/core_spec.clj
@@ -1,79 +1,92 @@
 (ns gilded-rose.core-spec
-(:require [speclj.core :refer :all]
-          [gilded-rose.core :refer :all]))
+ (:require [speclj.core :refer :all]
+           [gilded-rose.core :refer :all]))
+
+(def sulfuras (make-sulfuras 0 80))
+(def updated-sulfuras (update sulfuras))
+(println updated-sulfuras)
+
+(def default-item (item "Default Item" 10 20))
+(def updated-sell-in (update-sell-in default-item))
+(def updated-quality (update-item-quality default-item))
+(def updated-item (update default-item))
+
+(def inventory [sulfuras])
+(def updated-inventory (update-inventory inventory))
 
 (describe "gilded rose"
-  (describe "item"
-    (let [default-item (item "Default Item" 10 20)]
+  (context "item"
+
       (it "has a name"
-        (should= (default-item :name) "Default Item"))
+        (should-not-be-nil (default-item :name)))
 
       (it "has a sell-in value"
-        (should= (default-item :sell-in) 10))
+        (should-not-be-nil (default-item :sell-in)))
 
       (it "has a quality"
-        (should= (default-item :quality) 20))
+        (should-not-be-nil (default-item :quality)))
 
-      (describe "#update-sell-in"
-        (it "decreases the sell-in value"
-          (let [updated-item (update-sell-in default-item)]
-            (should= (:sell-in updated-item) 9))))
+     (context "Sulfuras"
+        (it "has a name"
+            (should= (sulfuras :name) "Sulfuras, Hand of Ragnaros"))))
 
-      (describe "#update-quality"
-        (it "decreases the quality"
-          (let [updated-item (update-quality default-item)]
-            (should= (:quality updated-item) 19))))))
+  (context "#update-sell-in"
+    (it "decreases the sell-in value"
+      (should= (:sell-in updated-sell-in) 9)))
 
-  (describe "Sulfuras"
-    (let [sulfuras-item (sulfuras 0 80)]
-      (it "never decreases sell-in value"
-        (let [updated-sulfuras (update-sell-in sulfuras)]
-          (should= (:sell-in updated-sulfuras) 0)))
+  (context "#update-quality"
+    (it "decreases the quality"
+      (should= (:quality updated-quality) 19)))
 
-      (it "never decreases in quality"
-        (let [updated-sulfuras (update-quality sulfuras)]
-          (should= (:quality updated-sulfuras) 80)))
+  (context "#update"
+     (it "updates the quality and sell-in"
+         (should= (:sell-in updated-item) 9)
+         (should= (:quality updated-item) 19))
 
-      ))
+    (context "Sulfuras"
+      (it "never decreases the quality"
+          (should= (:quality updated-sulfuras) 80))
+      (it "never decreases the sell-in"
+          (should= (:sell-in updated-sulfuras) 0)))))
 
-  (describe "update current inventory"
-    (let [updated-inventory (update-current-inventory)]
-      (let [
-        dexterity-vest (nth updated-inventory 0)
-        aged-brie (nth updated-inventory 1) elixir-mongoose (nth updated-inventory 2)
-        sulfuras (nth updated-inventory 3) backstage-passes (nth updated-inventory 4)]
+  ; (describe "update current inventory"
+  ;   (let [updated-inventory (update-current-inventory)]
+  ;     (let [
+  ;           dexterity-vest (nth updated-inventory 0)
+  ;           aged-brie (nth updated-inventory 1) elixir-mongoose (nth updated-inventory 2)
+  ;           sulfuras (nth updated-inventory 3) backstage-passes (nth updated-inventory 4)]
+  ;
+  ;       (describe "default items"
+  ;         (it "lowers the sell-in value by 1"
+  ;           (should= (dexterity-vest :sell-in) 9)
+  ;           (should= (aged-brie :sell-in) 1)
+  ;           (should= (elixir-mongoose :sell-in) 4)
+  ;           (should= (backstage-passes :sell-in) 14))
+  ;
+  ;         (it "lowers the quality by 1 if sell-in has not passed"
+  ;           (should= (dexterity-vest :quality) 19)
+  ;           (should= (elixir-mongoose :quality) 6))
+  ;
+  ;         (it "lowers the quality by 2 if sell-in has passed"
+  ;           (let [old-dexterity-vest (item "+5 Dexterity Vest" 0 10)]
+  ;             (do (update-quality [old-dexterity-vest])
+  ;              (should= (old-dexterity-vest :quality) 8))))
+  ;
+  ;         (it "does not allow the quality to be greater than 50"
+  ;           (let [super-aged-brie (item "Aged Brie" 0 50)]
+  ;             (do (update-quality [super-aged-brie])
+  ;              (should= (super-aged-brie :quality) 50))))
+  ;
+  ;         (it "does not allow the quality to be negative"
+  ;           (let [old-elixir (item "Elixir of the Mongoose" 0 0)]
+  ;             (do (update-quality [old-elixir])
+  ;              (should= (old-elixir :quality) 0)))))
+  ;
+  ;
+  ;       (describe "Aged Brie"
+  ;         (it "increases in quality"
+  ;           (should= (aged-brie :quality) 1)))))))
 
-        (describe "default items"
-          (it "lowers the sell-in value by 1"
-            (should= (dexterity-vest :sell-in) 9)
-            (should= (aged-brie :sell-in) 1)
-            (should= (elixir-mongoose :sell-in) 4)
-            (should= (backstage-passes :sell-in) 14))
-
-          (it "lowers the quality by 1 if sell-in has not passed"
-            (should= (dexterity-vest :quality) 19)
-            (should= (elixir-mongoose :quality) 6))
-
-          (it "lowers the quality by 2 if sell-in has passed"
-            (let [old-dexterity-vest (item "+5 Dexterity Vest" 0 10)]
-              (do (update-quality [old-dexterity-vest])
-              (should= (old-dexterity-vest :quality) 8))))
-
-          (it "does not allow the quality to be greater than 50"
-            (let [super-aged-brie (item "Aged Brie" 0 50)]
-              (do (update-quality [super-aged-brie])
-              (should= (super-aged-brie :quality) 50))))
-
-          (it "does not allow the quality to be negative"
-            (let [old-elixir (item "Elixir of the Mongoose" 0 0)]
-              (do (update-quality [old-elixir])
-              (should= (old-elixir :quality) 0))))
-        )
-
-        (describe "Aged Brie"
-          (it "increases in quality"
-            (should= (aged-brie :quality) 1))
-        )
 
         ; (describe "Backstage passes"
         ;   ;these tests do not pass
@@ -95,4 +108,3 @@
         ;       (do (update-quality [backstage-passes-0])
         ;       (should= (backstage-passes-0 :quality) 0))))
         ; )
-))))
--- src/gilded_rose/core.clj
+++ src/gilded_rose/core.clj
@@ -4,82 +4,89 @@
 (defn item [item-name, sell-in, quality]
   {:name item-name, :sell-in sell-in, :quality quality})
 
-(defmulti update-sell-in :Item)
+(defmulti update-sell-in :item)
+
+(defmulti update-item-quality :item)
 
 (defmethod update-sell-in :default [item]
   (merge item {:sell-in (dec (:sell-in item))}))
 
-(defmulti update-item-quality :Item)
-
 (defmethod update-item-quality :default [item]
   (merge item {:quality (dec (:quality item))}))
 
-(defn sulfuras [sell-in, quality]
-  {:Item :Sulfuras :name "Sulfuras, Hand of Ragnaros", :sell-in sell-in, :quality quality})
+(defn make-sulfuras [sell-in, quality]
+  {:item :sulfuras :name "Sulfuras, Hand of Ragnaros", :sell-in sell-in, :quality quality})
 
-(defmethod update-sell-in :Sulfuras [sulfuras]
-  (sulfuras))
+(defmethod update-sell-in :sulfuras [sulfuras]
+  sulfuras)
 
-(defmethod update-item-quality :Sulfuras [sulfuras]
-  (sulfuras))
+(defmethod update-item-quality :sulfuras [sulfuras]
+  sulfuras)
+
+(defn update [item]
+  (let [updated-item (update-sell-in item)]
+    (update-item-quality updated-item)))
+
+(defn update-inventory [items]
+  (map update items))
+
 
 (defn update-quality [items]
   (map
     (fn[item] (cond
       ; if sell-in is less than 0 and item is backstage passes
-      (and (< (:sell-in item) 0) (= "Backstage passes to a TAFKAL80ETC concert" (:name item)))
+               (and (< (:sell-in item) 0) (= "Backstage passes to a TAFKAL80ETC concert" (:name item)))
         ; quality = 0
-        (merge item {:quality 0})
+               (merge item {:quality 0})
       ; if item is aged brie or backstage passes
-      (or (= (:name item) "Aged Brie") (= (:name item) "Backstage passes to a TAFKAL80ETC concert"))
+               (or (= (:name item) "Aged Brie") (= (:name item) "Backstage passes to a TAFKAL80ETC concert"))
         ; if item is backstage passes and sell-in is between 5 and 9
-        (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 5) (< (:sell-in item) 10))
+               (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 5) (< (:sell-in item) 10))
           ; quality increases by 2
-          (merge item {:quality (inc (inc (:quality item)))})
+                 (merge item {:quality (inc (inc (:quality item)))})
           ; else if item is backstage passes and sell-in is between 0 and 4
-          (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 0) (< (:sell-in item) 5))
+                 (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 0) (< (:sell-in item) 5))
             ; quality increases by 3
-            (merge item {:quality (inc (inc (inc (:quality item))))})
+                   (merge item {:quality (inc (inc (inc (:quality item))))})
             ; else if item quality is less than 50
-            (if (< (:quality item) 50)
+                   (if (< (:quality item) 50)
               ; quality increases by 1
-              (merge item {:quality (inc (:quality item))})
-              item)))
+                     (merge item {:quality (inc (:quality item))})
+                     item)))
       ; if item sell-in is less than 0
-      (< (:sell-in item) 0)
+               (< (:sell-in item) 0)
         ; if item is backstage passes
-        (if (= "Backstage passes to a TAFKAL80ETC concert" (:name item))
+               (if (= "Backstage passes to a TAFKAL80ETC concert" (:name item))
           ; qualty = 0
-          (merge item {:quality 0})
+                 (merge item {:quality 0})
           ; if item is dexterity vest or elixir of the mongoose
-          (if (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
+                 (if (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
             ; quality decreases by 2
-            (merge item {:quality (- (:quality item) 2)})
-            item))
+                   (merge item {:quality (- (:quality item) 2)})
+                   item))
       ; if item is exterity vest or elixr of the mongoose
-      (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
+               (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
         ;quality decreases by 1
-        (merge item {:quality (dec (:quality item))})
+               (merge item {:quality (dec (:quality item))})
       ; for all other items
-      :else item))
-  (map (fn [item]
+               :else item))
+   (map (fn [item]
       ; if item is not sulfuras
-      (if (not= "Sulfuras, Hand of Ragnaros" (:name item))
+         (if (not= "Sulfuras, Hand of Ragnaros" (:name item))
         ; decrease sell-in
-        (merge item {:sell-in (dec (:sell-in item))})
-        item))
-  items)))
+           (merge item {:sell-in (dec (:sell-in item))})
+           item))
+    items)))
 
 
 ; starting values for inventory
 (defn update-current-inventory[]
   (let [inventory
-    [
-      (item "+5 Dexterity Vest" 10 20)
-      (item "Aged Brie" 2 0)
-      (item "Elixir of the Mongoose" 5 7)
-      (item "Sulfuras, Hand Of Ragnaros" 0 80)
-      (item "Backstage passes to a TAFKAL80ETC concert" 15 20)
-    ]]
-    (update-quality inventory)
-    ))
+        [
+          (item "+5 Dexterity Vest" 10 20)
+          (item "Aged Brie" 2 0)
+          (item "Elixir of the Mongoose" 5 7)
+          (make-sulfuras 0 80)
+          (item "Backstage passes to a TAFKAL80ETC concert" 15 20)]]
+
+    (update-quality inventory)))
